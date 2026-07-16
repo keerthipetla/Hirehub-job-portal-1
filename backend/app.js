c4e9aa58343dd16aaf39e6767ac0f5937a08d336
Keerthi Petla <petlakeerthi@gmail.com>
2026-07-16 23:09:02 +0530

    Fix CORS configuration

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,8 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
