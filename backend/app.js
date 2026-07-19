8768eed7421ad6e74f3651ecc5fe101b29832649
Keerthi Petla <petlakeerthi@gmail.com>
2026-07-19 16:47:47 +0530

    Allow localhost and Vercel in CORS

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,20 @@ import fileUpload from "express-fileupload";
 const app = express();
 config({ path: "./config/config.env" });
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://hirehub-job-portal-eight.vercel.app",
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
